6836ca3175a429f6c79c3e8eb3540c9a5cec6483
Randy23123 <rlopez@appdev.yearup.org>
2023-10-18 15:56:29 -0400

    half done

--- src/main/java/com/pluralsight/SearchInventory.java
+++ src/main/java/com/pluralsight/SearchInventory.java
@@ -1,7 +1,21 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class SearchInventory {
     public static void main(String[] args) {
+        ArrayList<Product> inventory = getInventory();
+        inventory.add(new Product(1,"banana", .45));
+        inventory.add(new Product(2, "apple",.25));
+        inventory.add(new Product(3, "grapes", 3.25));
+        inventory.add(new Product(4, "jalapeno", .12));
+        inventory.add(new Product(5, "beans", 2.85));
+        Scanner scanner = new Scanner(System.in);
+
         
+    }
+
+    private static ArrayList<Product> getInventory() {
     }
 }
