8f7c6da56011835990a925c91448136c5eec6c9e
Randy23123 <rlopez@appdev.yearup.org>
2023-10-19 13:34:41 -0400

    finish

--- src/main/java/com/pluralsight/SearchInventory.java
+++ src/main/java/com/pluralsight/SearchInventory.java
@@ -2,14 +2,14 @@ package com.pluralsight;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class SearchInventory {
     public static void main(String[] args) throws IOException {
         ArrayList<Product> inventory = getInventory();
         FileReader fileReader = new FileReader("src/main/resources/inventory.csv");
         BufferedReader buffReader = new BufferedReader(fileReader);
-        String input = "";
+        String input;
+
         String productName;
         int productID;
         double productPrice;
@@ -20,7 +20,6 @@ public class SearchInventory {
             productPrice = Double.parseDouble(temp[2]);
             inventory.add(new Product(productID, productName, productPrice));
         }
-        Scanner scanner = new Scanner(System.in);
         System.out.println("We carry: ");
 
         for (int i = 0; i < inventory.size(); i++) {
@@ -28,9 +27,7 @@ public class SearchInventory {
             System.out.printf("id: %d %s - Price: $%.2f\n", p.getId(), p.getName(), p.getPrice());
         }
     }
-
     private static ArrayList<Product> getInventory() {
-        ArrayList<Product> inventory = new ArrayList<Product>();
-        return inventory;
+        return new ArrayList<>();
     }
 }
