f91a6009f36d1ca70133be4950526faf762491db
Randy23123 <rlopez@appdev.yearup.org>
2023-10-19 09:32:12 -0400

    done with ex 1

--- src/main/java/com/pluralsight/SearchInventory.java
+++ src/main/java/com/pluralsight/SearchInventory.java
@@ -6,16 +6,22 @@ import java.util.Scanner;
 public class SearchInventory {
     public static void main(String[] args) {
         ArrayList<Product> inventory = getInventory();
-        inventory.add(new Product(1,"banana", .45));
-        inventory.add(new Product(2, "apple",.25));
+        inventory.add(new Product(1, "banana", .45));
+        inventory.add(new Product(2, "apple", .25));
         inventory.add(new Product(3, "grapes", 3.25));
         inventory.add(new Product(4, "jalapeno", .12));
         inventory.add(new Product(5, "beans", 2.85));
         Scanner scanner = new Scanner(System.in);
+        System.out.println("We carry: ");
 
-        
+        for (int i = 0; i < inventory.size(); i++) {
+            Product p = inventory.get(i);
+            System.out.printf("id: %d %s - Price: $%.2f\n", p.getId(), p.getName(), p.getPrice());
+        }
     }
 
     private static ArrayList<Product> getInventory() {
+        ArrayList<Product> inventory = new ArrayList<Product>();
+        return inventory;
     }
 }
